f68939346122ce7740a695f95f9c582e07647f00
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-04 13:04:59 -0400

    lecture work-in-progress

--- pokemon/src/components/Poki.js
+++ pokemon/src/components/Poki.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Poki extends React.Component {
   componentDidMount() {
-    console.log("Poki: Component Mounts");
+    console.log(`Poki ${pokemon.name}: Component Mounts`);
   }
 
   componentDidUpdate() {
@@ -11,7 +11,7 @@ class Poki extends React.Component {
 
   render() {
       const { pokemon } = this.props;
-      console.log("Poki: Component Renders");
+      console.log(`Poki ${pokemon.name}: Component Renders`);
       return(
       <div key={pokemon.id} className="pokemon">
         <img src={pokemon.img} alt={pokemon.name} />
