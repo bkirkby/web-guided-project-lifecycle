07c98f45d0d5ef0b8bd2ce68097edfe6013bf173
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-04 13:36:44 -0400

    lecture work-in-progress

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -9,8 +9,8 @@ class App extends React.Component {
                 <input/>
                 <button>Fetch Dogs</button>
             </form>
-            <div>
-
+            <div className="doggos">
+                
             </div>
         </div>);
     }
