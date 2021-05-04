fc11727ee84c21646e171dcb8c831972dbbc7d08
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-04 13:08:44 -0400

    lecture work-in-progress

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -7,11 +7,12 @@ import Pokemon from './components/Pokemon';
 
 class App extends React.Component {
   state = {
-    pokemon: data
+    pokemon: []
   }
 
   componentDidMount() {
     console.log("App: Component Mounts");
+    
   }
 
   componentDidUpdate() {
