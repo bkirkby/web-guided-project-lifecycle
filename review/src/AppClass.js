a967061ef8b473c3dda47c3ea8f9d619bc87f872
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-04 12:41:39 -0400

    lecture work-in-progress

--- review/src/AppClass.js
+++ review/src/AppClass.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 class AppClass extends React.Component {
+  // constructor() {
+  //   console.log("AppClass: Component Loads State");
+  //   super();
+  //   this.state = {
+  //     name: "Warren"
+  //   };
+  // }
+
   state = {
     name: "Warren"
   }
