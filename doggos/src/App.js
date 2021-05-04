3d9487780ed68527e849ceefb430b54eb2c168a1
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-04 13:43:44 -0400

    lecture work-in-progress

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -7,18 +7,6 @@ class App extends React.Component {
             "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
             "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
             "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
             "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg"
         ];
 
