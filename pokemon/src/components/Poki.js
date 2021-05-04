d6a3bf7c3f13e67a2b64e3d9b3ac75e33448dbe9
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-04 13:03:25 -0400

    lecture work-in-progress

--- pokemon/src/components/Poki.js
+++ pokemon/src/components/Poki.js
@@ -1,19 +1,22 @@
 import React from 'react';
 
 class Poki extends React.Component {
-    render() {
-        const { pokemon } = this.props;
-
-        return(
-        <div key={pokemon.id} className="pokemon">
-          <img src={pokemon.img} alt={pokemon.name} />
-          <div>
-            <h3>{pokemon.name}</h3>
-            {pokemon.next_evolution &&
-              pokemon.next_evolution.map(e => <p key={e.num}>{e.name}</p>)}
-          </div>
-        </div>);
-    }
+  componentDidMount() {
+    console.log("Poki: Component Mounts");
+  }
+  render() {
+      const { pokemon } = this.props;
+      console.log("Poki: Component Renders");
+      return(
+      <div key={pokemon.id} className="pokemon">
+        <img src={pokemon.img} alt={pokemon.name} />
+        <div>
+          <h3>{pokemon.name}</h3>
+          {pokemon.next_evolution &&
+            pokemon.next_evolution.map(e => <p key={e.num}>{e.name}</p>)}
+        </div>
+      </div>);
+  }
 }
 
 export default Poki;
