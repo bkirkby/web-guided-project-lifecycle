046cae65e562d62088d6be91806794e27bbb011f
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-04 13:40:38 -0400

    lecture work-in-progress

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -5,10 +5,12 @@ class App extends React.Component {
     render() {
         return(<div className="App">
             <h1>Dog Search Api v1.0</h1>
+            
             <form>
                 <input/>
                 <button>Fetch Dogs</button>
             </form>
+
             <div className="doggos">
                 <img width='200' src="https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg" alt="https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg"/>
                 <img width='200'src="https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg" alt="https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg"/>
