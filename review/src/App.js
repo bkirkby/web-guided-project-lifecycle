3651021ef29a0f33f536992ceddefb0ecbde8cde
kirkby <kirkby@neighbor.com>
2021-05-06 20:33:33 -0600

    progress!!

--- review/src/App.js
+++ review/src/App.js
@@ -54,7 +54,7 @@ class App extends React.Component {
       <div>
         <h1>hello {this.state.name}</h1>
         <button onClick={this.handleClick}>make it brian</button>
-        {this.state.name === 'brian' ? <SomeComp /> : <></>}
+        {this.state.name !== 'brian' ? <SomeComp /> : <></>}
       </div>
     )
   }
