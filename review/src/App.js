a055077bd12e09e14194029a84aedfa88028681f
kirkby <kirkby@neighbor.com>
2021-05-06 20:03:38 -0600

    progress!!

--- review/src/App.js
+++ review/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 class SomeComp extends React.Component {
-
   render() {
     return (
       <div>this is a different component</div>
@@ -30,6 +29,7 @@ class App extends React.Component {
       <div>
         <h1>hello {this.state.name}</h1>
         <button onClick={this.handleClick}>make it brian</button>
+        <SomeComp />
       </div>
     )
   }
