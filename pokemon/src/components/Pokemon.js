0fee5732df2e8026b969a6dd31d4de5b2af07243
kirkby <kirkby@neighbor.com>
2021-05-06 20:59:27 -0600

    progress!!

--- pokemon/src/components/Pokemon.js
+++ pokemon/src/components/Pokemon.js
@@ -6,10 +6,10 @@ function Pokemon(props) {
   const [myVar, setMyVar] = useState("this is a value");
   return (
     <>
+      <div>{myVar}</div>
       {props.pokemon.length === 0 ? <div>there are no pokemon!</div> : props.pokemon.map(poki => (
         <Poki poki={poki} />
       ))}
-      <div>{myVar}</div>
     </>
   );
 }
