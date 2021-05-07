c3a8eae7f5b2440f327815fa101fff03bf9f90d8
kirkby <kirkby@neighbor.com>
2021-05-06 21:09:11 -0600

    progress!!

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -4,9 +4,18 @@ import axios from 'axios';
 // https://dog.ceo/api/breed/hound/images/random/3
 
 class App extends React.Component {
+  state = {
+    dogs: []
+  }
+
   render() {
     return (
-      <div><h1>some nice doggos!</h1></div>
+      <div>
+        <h1>some nice doggos!</h1>
+        {this.state.dogs.map(dogImageUrl => {
+          return <img alt="dog image" src={dogImageUrl} key={dogImageUrl} />
+        })}
+      </div>
     )
   }
 }
