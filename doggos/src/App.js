c591852dbb90f8dc99d3302b0969995e6fe51d16
kirkby <kirkby@neighbor.com>
2021-05-06 21:02:33 -0600

    progress!!

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import axios from 'axios';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div><h1>some nice doggos!</h1></div>
+    )
+  }
+}
